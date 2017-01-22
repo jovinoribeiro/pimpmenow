08949b1965be1931411779cc1323f5dfebefca71
Jovino <jovribeiro@gmail.com>
2017-01-22 15:43:36 -0800

    add push

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pimp Me UP - Facebook App' });
+  res.render('index', { title: 'Pimp Me UP - Facebook App 1' });
 });
 
 router.post('/', function(req, res, next) {
